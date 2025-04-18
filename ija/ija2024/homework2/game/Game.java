9cc8c99dc7c3b096ea6ee65ecd4a0f4e35109958
tomasuda <xsudato00@stud.fit.vutbr.cz>
2025-04-18 11:00:22 +0200

    done

--- ija/ija2024/homework2/game/Game.java
+++ ija/ija2024/homework2/game/Game.java
@@ -2,332 +2,143 @@
  * @author xsudato00
  */
 
-package ija.ija2024.homework2.common;
-
-import ija.ija2024.homework2.game.Game;
-import java.util.Arrays;
-
-import java.lang.reflect.Array;
-
-public class GameNode {
-    public enum nodeType {
-        NONE, BULB, POWER, LINK
-    }
-    private nodeType type;
-    Position position;
-    public Side[] sides;
-    public static int linkCount=0,bulbCount=0,powerCount =0;
-    private Game game;
-    public static Position LastPosition;
-    public GameNode(){
-        this.type=nodeType.NONE;
-        this.sides = new Side[4];
-    }
-
-    public Position getPosition(){return this.position;}
-
-    public void setGame(Game game) {
-        this.game = game;
-    }
-
-    public void turn(){
-        for (int i = 0; i < sides.length; i++) {
-            if (sides[i] != null) {
-                // Získajte aktuálny index strany
-                int currentOrdinal = sides[i].ordinal();
-                // Vypočítajte nový index (otáčanie o 90 stupňov)
-                int newOrdinal = (currentOrdinal + 1) % Side.values().length;
-                // Nastavte novú stranu
-                sides[i] = Side.values()[newOrdinal];
-            }
-        }
-    }
-
-    public boolean isLink(){
-        return type==nodeType.LINK;
-    }
-
-    public boolean isBulb(){
-        return type==nodeType.BULB;
-    }
-
-    public boolean isPower(){
-        return type==nodeType.POWER;
-    }
-
-    public boolean containsConnector(Side s){
-        for (Side side : sides) {
-            if (side == s) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void setPosition(Position p){
-        this.position =  new Position(p);
-    }
-
-    public void setAsBulb(Side sides){
-        GameNode.bulbCount++;
-        this.type = nodeType.BULB;
-        this.sides[0] = sides; // Uloženie konektora
-    }
-
-    public void setAsLink(Side... sides){
-        GameNode.linkCount++;
-        this.type = nodeType.LINK;
-        // Uloženie konektorov do poľa
-        for (int i = 0; i < sides.length && i < this.sides.length; i++) {
-            // this.sides[i] = sides[i];
-            this.sides[sides[i].ordinal()] = sides[i];
-        }
-    }
-
-    public void setAsPower(Side... sides){
-        GameNode.powerCount++;
-        this.type = nodeType.POWER;
-        // Uloženie konektorov do poľa
-        for (int i = 0; i < sides.length && i < 4; i++) {
-            // this.sides[i] = sides[i];
-            this.sides[sides[i].ordinal()] = sides[i];
-        }
-
-    }
-
-    public void setAsNone(){
-        this.type = nodeType.NONE;
-        // Uloženie konektorov do poľa
-        this.sides[0] = Side.EAST;
-        this.sides[1] = Side.NORTH;
-        this.sides[2] = Side.SOUTH;
-        this.sides[3] = Side.WEST;
-    }
-
-    private Position moveToPosition(Side side,Position currentPos) {
-
-
-        if (currentPos == null) return null;
-
-        int newRow = currentPos.getRow();
-        int newCol = currentPos.getCol();
-
-        switch (side) {
-            case NORTH -> newRow--;
-            case EAST -> newCol++;
-            case SOUTH -> newRow++;
-            case WEST -> newCol--;
-            default -> { return null; }
-        }
-
-
-
-        return new Position(newRow+1, newCol+1);
-    }
-
-
-    public Boolean conected( Position prevPos,Position currentPos,Side conectorSide){
-
-        GameNode neighbor = game.node(currentPos);
-
-
-
-        if(neighbor.isPower()) {
-
-            boolean active_conector =false;
-
-            for (Side side : neighbor.sides) {
-                if(conectorSide!=null) {
-                    switch (conectorSide) {
-                        case EAST:
-                            if (side == Side.WEST) {
-                                active_conector = true;
-                                return true;
-                            }
-                            break ;
-                        case NORTH:
-                            if (side == Side.SOUTH) {
-                                active_conector = true;
-                                return true;
-                            }
-
-                            break;
-                        case SOUTH:
-                            if (side == Side.NORTH) {
-                                active_conector = true;
-                                return true;
-                            }
-                            break;
-                        case WEST:
-                            if (side == Side.EAST) {
-                                active_conector = true;
-                                return true;
-                            }
-                            break;
-                        default:
-
-                            //ERROR
-                            return false;
-
-                    }
-                }
-                else{
-                    return true;
-                }
-
-            }
-
-            if (!active_conector) {
-                return false;
-            }
-
-            return true;
-        }
-        else if(neighbor.isBulb() || neighbor.isLink()) {//        for (Side side : sides) {
-
-            boolean active_conector =false;
-            side_iteration:
-            for (Side side : neighbor.sides) {
-
-                if (conectorSide != null) {
-
-                    switch (conectorSide) {
-                        case EAST:
-                            if (side == Side.WEST) {
-                                active_conector = true;
-                                break  side_iteration;
-                            }
-                            continue;
-                        case NORTH:
-                            if (side == Side.SOUTH) {
-                                active_conector = true;
-
-                                break side_iteration;
-                            }
-                            continue;
-                        case SOUTH:
-                            if (side == Side.NORTH) {
-                                active_conector = true;
-                                break  side_iteration;
-                            }
-                            continue;
-                        case WEST:
-                            if (side == Side.EAST) {
-                                active_conector = true;
-                                break  side_iteration;
-                            }
-                            continue;
-                        default:
-
-                            //ERROR
-                            return false;
-
-                    }
-                }
-                else {
-                    active_conector = true;
-                }
-            }
-            if (!active_conector) {
-                return false;
-            }
-            Position tmp;
-            for (Side side : neighbor.sides) {
-
-                if (side !=null) {
-
-
-                     tmp =currentPos;
-                    currentPos = this.moveToPosition(side, currentPos);
-                    if (prevPos != null) {
-
-
-                        if (prevPos.getCol() == currentPos.getCol() && prevPos.getRow() == currentPos.getRow()) {
-
-                            currentPos = tmp;
-                            continue;
-                        }
-                    }
-
-                    if (currentPos == null) {
-                        continue;
-                    }
-
-
-                    prevPos = tmp;
-                    if (conected(prevPos, currentPos, side)) {
-                        prevPos= currentPos;
-                        return true;
-                    } else {
-
-                        prevPos= currentPos;
-                        currentPos=tmp;
-
-                    }
-
-                } else {
-
-                    continue;
-                }
-            }
-        }
-        else {
-            return false;
-        }
-
-    return false;
-
-    }
-
-    public boolean light(){
-        boolean result;
-        Position pos = this.getPosition();
-
-        this.LastPosition = new Position(-1, -1);
-
-
-        if( this.type.name()== "NONE")
-            result= false;
-        else {
-
-            result = conected(null, this.getPosition(), null);
-        }
-
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        // Určenie typu uzlu
-        char typeChar;
-        switch (this.type) {
-            case BULB: typeChar = 'B'; break;
-            case LINK: typeChar = 'L'; break;
-            case POWER: typeChar = 'P'; break;
-            default: typeChar = 'E'; // NONE
-        }
-
-        // Formátovanie pozície
-        String positionStr = position != null ?
-                "[" + (position.getRow()+1) + "@" + (position.getCol()+1) + "]" :
-                "[null@null]";
-
-        // Zoznam konektorov
-        StringBuilder connectors = new StringBuilder("[");
-        boolean first = true;
-
-        // Prechádzame všetky možné strany v poradí NORTH, EAST, SOUTH, WEST
-        for (Side side : Side.values()) {
-            if (containsConnector(side)) {
-                if (!first) {
-                    connectors.append(",");
-                }
-                connectors.append(side.name());
-                first = false;
-            }
-        }
-        connectors.append("]");
-
-        return "{" + typeChar + positionStr + connectors + "}";
-    }
-}
+ package ija.ija2024.homework2.game;
+
+ import ija.ija2024.homework2.common.GameNode;
+ import ija.ija2024.homework2.common.Position;
+ import ija.ija2024.homework2.common.Side;
+ 
+ /**
+  * Třída reprezentující prostředí hry s mřížkou políček.
+  */
+ public class Game {
+     public  int rows;
+     public  int cols;
+     private GameNode[][] board;
+     private boolean hasPowerNode = false;
+ 
+     public Game(int rows, int cols){
+         this.rows = rows;
+         this.cols = cols;
+         this.board = new GameNode[rows][cols];
+ 
+ 
+         for (int r = 0; r < rows; r++) {
+             for (int c = 0; c < cols; c++) {
+                 this.board[r][c] = new GameNode();
+                 this.board[r][c].setGame(this);
+             }
+         }
+     }
+ 
+     public Game() {
+ 
+     }
+ 
+     public static Game create(int rows, int cols) {
+         GameNode.linkCount=GameNode.powerCount=GameNode.bulbCount=0;
+         if( (rows < 1) ||( cols < 1)){
+             throw new IllegalArgumentException("Argumenty nenej alebo rovno nula su nepovolene");
+         }
+         return new Game(rows,cols);
+     }
+ 
+     public GameNode createLinkNode(Position p,Side... sides){
+         if ((p.getRow() >= rows() || p.getRow() < 0 )||( p.getCol() >= cols() ||  p.getCol() < 0)||sides.length<2){
+             return null;
+         }
+ 
+         GameNode gn = node(p);
+         gn.setGame(this);
+         gn.setPosition(p);
+         gn.setAsLink(sides);
+         return  gn;
+     }
+ 
+     public GameNode createPowerNode(Position p,Side... sides){
+         if (((p.getRow() >= rows() || p.getRow() < 0 )||( p.getCol() >= cols() ||  p.getCol() < 0))|| sides.length<1 ){
+             return null;
+         }
+ 
+         if (GameNode.powerCount >= 1){
+             return null;
+         }
+         GameNode gn = node(p);
+         gn.setGame(this);
+         gn.setPosition(p);
+         gn.setAsPower(sides);
+         return  gn;
+     }
+ 
+     public GameNode createBulbNode(Position p,Side sides){
+         if ((p.getRow() >= rows() || p.getRow() < 0 )||( p.getCol() >= cols() ||  p.getCol() < 0)){
+             return null;
+         }
+         GameNode gn = node(p);
+         gn.setGame(this);
+         gn.setPosition(p);
+         gn.setAsBulb(sides);
+         return  gn;
+     }
+ 
+     public int rows(){return this.rows;}
+ 
+     public int cols(){ return this.cols;}
+ 
+     public GameNode node(Position p){
+         return this.board[p.getRow()][p.getCol()];
+     }
+ 
+     public void init() {
+         int powerCount = 0;
+         int bulbCount = 0;
+         GameNode powerNode = null;
+         Position powerPos = null;
+ 
+         // First pass: count power nodes and bulbs, and locate the power node
+         for (int r = 1; r <= rows(); r++) {
+             for (int c = 1; c <= cols(); c++) {
+                 Position pos = new Position(r, c);
+                 GameNode node = node(pos);
+ 
+                 if (node != null) {
+                     if (node.isPower()) {
+                         powerCount++;
+                         powerNode = node;
+                         powerPos = pos;
+                     } else if (node.isBulb()) {
+                         bulbCount++;
+                     }
+                 }
+             }
+         }
+ 
+         // Validate game rules
+         if (powerCount != 1) {
+             throw new IllegalStateException("Game must have exactly one power node, found " + powerCount);
+         }
+         if (bulbCount < 1) {
+             throw new IllegalStateException("Game must have at least one bulb, found " + bulbCount);
+         }
+ 
+         // Second pass: light up connected nodes starting from power node
+         if (!powerNode.light()) {
+             throw new IllegalStateException("Power node is not properly connected to any bulbs");
+         }
+ 
+ //        // Optional: Mark all lit nodes (if you want to track this separately)
+ //        for (int r = 0; r < rows(); r++) {
+ //            for (int c = 0; c < cols(); c++) {
+ //                Position pos = new Position(r, c);
+ //                GameNode node = node(pos);
+ //                if (node != null && node.light()) {
+ //                    // You could add node.setLit(true) here if you add this method
+ //                }
+ //            }
+ //        }
+     }
+ 
+ //    public GameNode getNode(Position currentPos) {
+ //        return this.board[currentPos.getRow()][currentPos.getCol()];
+ //    }
+ }
